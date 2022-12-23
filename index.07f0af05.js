9105a051144f18ea42c1130923d94c272d12eca6
OleksandrPM <114025110+OleksandrPM@users.noreply.github.com>
2022-12-23 19:16:34 +0000

    Deploying to gh-pages from @ OleksandrPM/ice-cream@3088bdb7cb6776c8fe106355c07651c075ecdef7 🚀

---
+++ index.07f0af05.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("backdrop--is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.07f0af05.js.map
