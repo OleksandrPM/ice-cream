fd5edc913a773793d15c3d1da745475ff633d573
OleksandrPM <114025110+OleksandrPM@users.noreply.github.com>
2022-12-28 16:30:11 +0000

    Deploying to gh-pages from @ OleksandrPM/ice-cream@c2e94fc0ef26bed28c06b3d7dd52e7226d2f1339 🚀

---
+++ index.807ec7e3.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector(".js-data-header"),a=e.offsetHeight;window.pageYOffset>a?e.classList.add("header--no-transparency"):e.classList.remove("header--no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.807ec7e3.js.map
